4ab11419ff0e4ac390918067f1fc90f93682445c
cmbothwell <corey.bothwell@icloud.com>
2024-05-26 19:51:29 -0400

    arrow integration

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from operator import itemgetter
 from typing import Any
 from pprint import PrettyPrinter
 
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
 pp = PrettyPrinter()
 
 parse_date = lambda x: datetime.strptime(x, "%m/%d/%y")
@@ -139,8 +143,48 @@ formatted_security_master = [
     for t in security_master
 ]
 header = [["security_id", "effective_start_date", "effective_end_date", *attributes]]
-print("Security Master")
-print(get_pretty_table([*header, *formatted_security_master]), "\n")
+# print("Security Master")
+# print(get_pretty_table([*header, *formatted_security_master]), "\n")
+
+# Pandas
+# print(list(zip(*security_master)))
+# security_master = pd.DataFrame(
+#     security_master,
+#     columns=["security_id", "effective_start_date", "effective_end_date", *attributes],
+# )
+
+# PyArrow
+cols = list(zip(*security_master))
+security_ids = pa.array(cols[0], type=pa.int8())
+effective_start_dates = pa.array(cols[1], type=pa.date64())
+effective_end_dates = pa.array(cols[2], type=pa.date64())
+asset_classes = pa.array(cols[3], type=pa.string())
+tickers = pa.array(cols[4], type=pa.string())
+names = pa.array(cols[5], type=pa.string())
+market_cap = pa.array(cols[6], type=pa.float64())
+gics_sectors = pa.array(cols[7], type=pa.string())
+gics_industries = pa.array(cols[8], type=pa.string())
+
+arrow_table = pa.table(
+    [
+        security_ids,
+        effective_start_dates,
+        effective_end_dates,
+        asset_classes,
+        tickers,
+        names,
+        market_cap,
+        gics_sectors,
+        gics_industries,
+    ],
+    names=["security_id", "effective_start_date", "effective_end_date", *attributes],
+)
+
+print(arrow_table)
+pq.write_table(arrow_table, "security_master.parquet")
+reloaded_table = pq.read_table("security_master.parquet")
+
+print(reloaded_table)
 
 
 joined_table = [
@@ -151,8 +195,8 @@ formatted_joined_table = [
     for t in security_master
 ]
 join_header = [["security_id", "quantity", "date", *attributes]]
-print("Consolidated Position Information")
-print(get_pretty_table([*join_header, *formatted_joined_table]), "\n")
+# print("Consolidated Position Information")
+# print(get_pretty_table([*join_header, *formatted_joined_table]), "\n")
 
 
 # Scratchpad
