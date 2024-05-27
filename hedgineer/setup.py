b31876d8a717ab3b96135e9a2455857282c4b354
cmbothwell <corey.bothwell@icloud.com>
2024-05-26 22:08:49 -0400

    local build info

---
+++ hedgineer/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hedgineer",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Corey Bothwell",
+    author_email="corey@bothwell.org",
+    description="Part of the Hedgineer take-home assessment",
+)
