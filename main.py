66c6ad7e11bf137e8a71f6099093c856966c55d4
cmbothwell <corey.bothwell@icloud.com>
2024-05-26 20:10:49 -0400

    pandas & testing

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from operator import itemgetter
 from typing import Any
 from pprint import PrettyPrinter
 
+import numpy as np
+import numpy.testing as npt
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pyarrow.csv as csv
 
 pp = PrettyPrinter()
 
@@ -147,44 +150,47 @@ header = [["security_id", "effective_start_date", "effective_end_date", *attribu
 # print(get_pretty_table([*header, *formatted_security_master]), "\n")
 
 # Pandas
-# print(list(zip(*security_master)))
-# security_master = pd.DataFrame(
-#     security_master,
-#     columns=["security_id", "effective_start_date", "effective_end_date", *attributes],
-# )
-
-# PyArrow
-cols = list(zip(*security_master))
-security_ids = pa.array(cols[0], type=pa.int8())
-effective_start_dates = pa.array(cols[1], type=pa.date64())
-effective_end_dates = pa.array(cols[2], type=pa.date64())
-asset_classes = pa.array(cols[3], type=pa.string())
-tickers = pa.array(cols[4], type=pa.string())
-names = pa.array(cols[5], type=pa.string())
-market_cap = pa.array(cols[6], type=pa.float64())
-gics_sectors = pa.array(cols[7], type=pa.string())
-gics_industries = pa.array(cols[8], type=pa.string())
-
-arrow_table = pa.table(
-    [
-        security_ids,
-        effective_start_dates,
-        effective_end_dates,
-        asset_classes,
-        tickers,
-        names,
-        market_cap,
-        gics_sectors,
-        gics_industries,
-    ],
-    names=["security_id", "effective_start_date", "effective_end_date", *attributes],
+security_master_pd = pd.DataFrame(
+    security_master,
+    columns=["security_id", "effective_start_date", "effective_end_date", *attributes],
 )
 
-print(arrow_table)
-pq.write_table(arrow_table, "security_master.parquet")
-reloaded_table = pq.read_table("security_master.parquet")
+security_master_pd_copy = security_master_pd.copy(deep=True)
+assert security_master_pd.equals(security_master_pd_copy)
 
-print(reloaded_table)
+# PyArrow
+# cols = list(zip(*security_master))
+# security_ids = pa.array(cols[0], type=pa.int8())
+# effective_start_dates = pa.array(cols[1], type=pa.date64())
+# effective_end_dates = pa.array(cols[2], type=pa.date64())
+# asset_classes = pa.array(cols[3], type=pa.string())
+# tickers = pa.array(cols[4], type=pa.string())
+# names = pa.array(cols[5], type=pa.string())
+# market_cap = pa.array(cols[6], type=pa.float64())
+# gics_sectors = pa.array(cols[7], type=pa.string())
+# gics_industries = pa.array(cols[8], type=pa.string())
+
+# arrow_table = pa.table(
+#     [
+#         security_ids,
+#         effective_start_dates,
+#         effective_end_dates,
+#         asset_classes,
+#         tickers,
+#         names,
+#         market_cap,
+#         gics_sectors,
+#         gics_industries,
+#     ],
+#     names=["security_id", "effective_start_date", "effective_end_date", *attributes],
+# )
+
+
+# pq.write_table(arrow_table, "security_master.parquet")
+# reloaded_table = pq.read_table("security_master.parquet")
+
+# csv.write_csv(reloaded_table, "security_master.csv")
+# reloaded_table = csv.read_csv("security_master.csv")
 
 
 joined_table = [
